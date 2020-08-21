777b20e7a039ec7e1bcacbf34123d4ba72190571
desktop <brhilyes@gmail.com>
2020-08-22 00:01:18 +0100

    small db shift

--- src/datasources/postegress-connector.datasource.ts
+++ src/datasources/postegress-connector.datasource.ts
@@ -1,25 +1,25 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
-// const config = {
-//   name: 'postegressConnector',
-//   connector: 'postgresql',
-//   host: 'ec2-176-34-114-78.eu-west-1.compute.amazonaws.com',
-//   port: 5432,
-//   user: 'ejzfodalsbvqqt',
-//   password: '8c7b7af3ab0c6804d3bbe8a90eeecdab6e171cdc563b771621da423b57a74841',
-//   database: 'dcgnan9dh22310'
-// };
-
 const config = {
   name: 'postegressConnector',
   connector: 'postgresql',
-  host: 'localhost',
+  host: 'ec2-176-34-114-78.eu-west-1.compute.amazonaws.com',
   port: 5432,
-  user: 'postgres',
-  password: 'admin',
-  database: 'owmenperfume'
+  user: 'ejzfodalsbvqqt',
+  password: '8c7b7af3ab0c6804d3bbe8a90eeecdab6e171cdc563b771621da423b57a74841',
+  database: 'dcgnan9dh22310'
 };
+
+// const config = {
+//   name: 'postegressConnector',
+//   connector: 'postgresql',
+//   host: 'localhost',
+//   port: 5432,
+//   user: 'postgres',
+//   password: 'admin',
+//   database: 'owmenperfume'
+// };
 
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
